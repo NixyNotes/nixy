4a481bae28d86d1f1e98d6a37d43640fbcf5e659
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-05-23 16:54:37 +0200

    feat: note view improvements

--- lib/features/note/controllers/note_view.controller.dart
+++ lib/features/note/controllers/note_view.controller.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:intl/intl.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nextcloudnotes/core/controllers/queue.controller.dart';
 import 'package:nextcloudnotes/main.dart';
 import 'package:nextcloudnotes/models/note.model.dart';
 import 'package:nextcloudnotes/repositories/notes.repositories.dart';
 
-part 'note-view.controller.g.dart';
+part 'note_view.controller.g.dart';
 
-@lazySingleton
+disposeNoteViewController(NoteViewController instance) {
+  instance.dispose();
+}
+
+@LazySingleton(dispose: disposeNoteViewController)
 class NoteViewController = _NoteViewControllerBase with _$NoteViewController;
 
 abstract class _NoteViewControllerBase with Store {
@@ -16,10 +21,40 @@ abstract class _NoteViewControllerBase with Store {
   final NoteRepositories _noteRepositories;
   final QueueController _queueController;
 
+  final FocusNode focusNode = FocusNode();
+
   TextEditingController markdownController = TextEditingController();
 
-  Future<Note> fetchNote(int noteId) {
-    return _noteRepositories.fetchNote(noteId);
+  @observable
+  bool isTextFieldFocused = false;
+
+  @observable
+  bool editMode = false;
+
+  @computed
+  String get prettyDate => DateFormat("yyyy-MM-dd HH:mm")
+      .format(DateTime.utc(1970, 1, 1).add(Duration(seconds: note.modified)));
+
+  late Note note;
+
+  init(int noteId) {
+    fetchNote(noteId);
+
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        isTextFieldFocused = true;
+      }
+    });
+  }
+
+  dispose() {
+    focusNode.dispose();
+  }
+
+  Future<Note> fetchNote(int noteId) async {
+    note = await _noteRepositories.fetchNote(noteId);
+
+    return note;
   }
 
   Future<bool> deleteNote(int noteId) async {
@@ -42,5 +77,16 @@ abstract class _NoteViewControllerBase with Store {
 
     _queueController.queue.sink.add(
         QueueAction(type: QueueActionTypes.UPDATE, note: Note.fromJson(note0)));
+  }
+
+  onTapDone(int noteId, Note note) {
+    focusNode.unfocus();
+    toggleEditMode();
+    isTextFieldFocused = false;
+    updateNote(noteId, note).then((value) => fetchNote(noteId));
+  }
+
+  toggleEditMode() {
+    editMode = !editMode;
   }
 }
--- lib/features/note/note.view.dart
+++ lib/features/note/note.view.dart
@@ -2,11 +2,11 @@ import 'package:auto_route/auto_route.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nextcloudnotes/components/future_builder.component.dart';
 import 'package:nextcloudnotes/core/services/di/di.dart';
 import 'package:nextcloudnotes/core/shared/components/scaffold.component.dart';
-import 'package:nextcloudnotes/features/note/controllers/note-view.controller.dart';
+import 'package:nextcloudnotes/features/note/controllers/note_view.controller.dart';
 import 'package:nextcloudnotes/models/note.model.dart';
 import 'package:nixi_markdown/formatters/newline_formatter.dart';
 
@@ -22,12 +22,17 @@ class NoteView extends StatefulWidget {
 
 class _NoteViewState extends State<NoteView> {
   final controller = getIt<NoteViewController>();
-  bool editMode = false;
-  GlobalKey key = GlobalKey();
+
   @override
   void initState() {
     super.initState();
-    controller.fetchNote(widget.noteId);
+    controller.init(widget.noteId);
+  }
+
+  @override
+  void dispose() {
+    getIt.resetLazySingleton<NoteViewController>();
+    super.dispose();
   }
 
   @override
@@ -47,34 +52,43 @@ class _NoteViewState extends State<NoteView> {
                         .then((value) => context.router.back());
                   },
                   icon: const Icon(EvaIcons.trash2Outline)),
+              Observer(
+                builder: (context) {
+                  if (controller.isTextFieldFocused) {
+                    return ElevatedButton(
+                        onPressed: () => controller.onTapDone(data!.id, data),
+                        child: const Text("Done"));
+                  }
+
+                  return const SizedBox.shrink();
+                },
+              )
             ],
-            body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SelectableText(data!.modified.toString()),
-                  Text(DateFormat("yyyy-MM-dd HH:mm").format(
-                      DateTime.utc(1970, 1, 1)
-                          .add(const Duration(seconds: 1684543279)))),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        editMode = !editMode;
-                      });
-                    },
-                    child: editMode
-                        ? TextField(
-                            maxLines: null,
-                            controller: controller.markdownController,
-                            inputFormatters: [NewlineFormatter()],
-                            onChanged: (value) {
-                              Future.delayed(const Duration(seconds: 2),
-                                  () => controller.updateNote(data.id, data));
-                            },
-                          )
-                        : Markdown(data: data.content),
-                  )
-                ],
-              ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: Observer(builder: (_) {
+                    return GestureDetector(
+                      onDoubleTap: controller.toggleEditMode,
+                      child: controller.editMode
+                          ? TextField(
+                              focusNode: controller.focusNode,
+                              autofocus: true,
+                              maxLines: null,
+                              controller: controller.markdownController,
+                              keyboardType: TextInputType.multiline,
+                              inputFormatters: [NewlineFormatter()],
+                            )
+                          : MarkdownBody(
+                              data: controller.markdownController.text,
+                              styleSheet: MarkdownStyleSheet(
+                                  checkbox: const TextStyle(
+                                      color: Colors.orangeAccent)),
+                            ),
+                    );
+                  }),
+                )
+              ],
             ));
       },
     );
