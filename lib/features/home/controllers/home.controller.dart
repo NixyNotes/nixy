0552613d85d83ec81d5f6359dae012b6b0c0a3db
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-05-24 02:35:26 +0200

    feat: add delete support

--- lib/features/home/controllers/home.controller.dart
+++ lib/features/home/controllers/home.controller.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:mobx/mobx.dart';
+import 'package:nextcloudnotes/core/services/toast.service.dart';
 import 'package:nextcloudnotes/models/note.model.dart';
 import 'package:nextcloudnotes/repositories/notes.repositories.dart';
 
@@ -9,14 +10,64 @@ part 'home.controller.g.dart';
 class HomeViewController = _HomeViewControllerBase with _$HomeViewController;
 
 abstract class _HomeViewControllerBase with Store {
-  _HomeViewControllerBase(this._noteRepositories);
+  _HomeViewControllerBase(this._noteRepositories, this._toastService);
   final NoteRepositories _noteRepositories;
+  final ToastService _toastService;
 
-  ObservableFuture<List<Note>>? notes;
+  @observable
+  ObservableList<Note> notes = ObservableList();
 
+  @observable
+  bool isLoading = false;
+
+  @observable
+  ObservableList<Note> selectedNotes = ObservableList();
+
+  @action
   fetchNotes() async {
-    final response = _noteRepositories.fetchNotes();
+    isLoading = true;
+    final response = await _noteRepositories.fetchNotes();
 
-    notes = ObservableFuture(response);
+    notes = ObservableList.of(response);
+    isLoading = false;
+  }
+
+  @action
+  deleteNote(Note note) async {
+    await _noteRepositories.deleteNote(note.id);
+
+    notes.removeWhere((element) => element.id == note.id);
+
+    _toastService.showTextToast("Deleted ${note.title}",
+        type: ToastType.success);
+  }
+
+  @action
+  bunchDeleteNotes() async {
+    final List<Future<bool>> futures = [];
+    for (var note in selectedNotes) {
+      final future = _noteRepositories.deleteNote(note.id);
+      futures.add(future);
+    }
+
+    Future.wait<bool>(futures).then((value) {
+      for (var note in selectedNotes) {
+        notes.removeWhere((element) => element.id == note.id);
+      }
+
+      _toastService.showTextToast("Deleted (${selectedNotes.length}) notes.",
+          type: ToastType.success);
+      selectedNotes.clear();
+    });
+  }
+
+  @action
+  addToSelectedNote(Note note) async {
+    if (selectedNotes.where((element) => element.id == note.id).isNotEmpty) {
+      selectedNotes.removeWhere((element) => element.id == note.id);
+      return;
+    }
+
+    selectedNotes.add(note);
   }
 }
--- lib/features/home/views/home.view.dart
+++ lib/features/home/views/home.view.dart
@@ -3,15 +3,21 @@ import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nextcloudnotes/core/controllers/auth.controller.dart';
-import 'package:nextcloudnotes/core/extensions/async_value.extension.dart';
 import 'package:nextcloudnotes/core/router/router.gr.dart';
 import 'package:nextcloudnotes/core/services/di/di.dart';
 import 'package:nextcloudnotes/core/shared/components/scaffold.component.dart';
 import 'package:nextcloudnotes/features/home/controllers/home.controller.dart';
 import 'package:nextcloudnotes/features/home/views/components/note_grid.component.dart';
-import 'package:nextcloudnotes/models/note.model.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:nextcloudnotes/models/note.model.dart';
 import 'package:pull_down_button/pull_down_button.dart';
+
+class HomeAction {
+  HomeAction({required this.label, this.isDestructive = false});
+
+  final String label;
+  final bool isDestructive;
+}
 
 @RoutePage()
 class HomeView extends StatefulWidget {
@@ -24,6 +30,12 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   final controller = getIt<HomeViewController>();
   final authStorage = getIt<AuthController>();
+
+  List<HomeAction> get actions => [
+        HomeAction(label: "Select"),
+        HomeAction(label: "Delete", isDestructive: true),
+      ];
+
   @override
   void initState() {
     super.initState();
@@ -37,116 +49,166 @@ class _HomeViewState extends State<HomeView> {
         bottomBar: _renderBottomBar(context),
         body: Observer(
           builder: (context) {
-            if (controller.notes == null) {
-              return const SizedBox.shrink();
+            if (controller.isLoading) {
+              return const Center(child: CircularProgressIndicator.adaptive());
             }
 
-            return controller.notes!.asyncValue<List<Note>>(
-              pending: () => const Center(
-                child: CircularProgressIndicator.adaptive(),
-              ),
-              rejected: (error) => Text(error.toString()),
-              fulfilled: (data) {
-                return RefreshIndicator.adaptive(
-                  onRefresh: () => controller.fetchNotes(),
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            mainAxisSpacing: 10,
-                            crossAxisSpacing: 10),
-                    itemCount: data.length,
-                    clipBehavior: Clip.antiAlias,
-                    itemBuilder: (BuildContext context, int index) {
-                      final note = data[index];
+            return RefreshIndicator.adaptive(
+              onRefresh: () => controller.fetchNotes(),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10),
+                itemCount: controller.notes.length,
+                clipBehavior: Clip.antiAlias,
+                itemBuilder: (BuildContext context, int index) {
+                  final note = controller.notes[index];
 
-                      return PlatformWidget(
-                        material: (context, platform) => GestureDetector(
-                          onLongPressDown: (details) {
-                            showMenu(
-                                context: context,
-                                position: RelativeRect.fromLTRB(
-                                    details.globalPosition.dx,
-                                    details.globalPosition.dy,
-                                    details.globalPosition.dx + 1,
-                                    details.globalPosition.dy + 1),
-                                items: [
-                                  const PopupMenuItem(child: Text("qwe"))
-                                ]);
+                  return PlatformWidget(
+                    material: (context, platform) => GestureDetector(
+                      onLongPressDown: (details) {
+                        showPlatformDialog(
+                          context: context,
+                          builder: (context) {
+                            return PlatformAlertDialog(
+                              actions: actions
+                                  .map((e) => PlatformDialogAction(
+                                        onPressed: () =>
+                                            controller.deleteNote(note),
+                                        child: Text(e.label),
+                                      ))
+                                  .toList(),
+                            );
                           },
-                          child: NoteGrid(
-                            title: note.title,
-                            content: note.content,
-                            date: note.modified,
-                            onTap: () => context.router
-                                .navigate(NoteRoute(noteId: note.id)),
-                          ),
-                        ),
-                        cupertino: (context, platform) {
-                          return PullDownButton(
-                              itemBuilder: (context) => [
-                                    PullDownMenuItem(
-                                      title: 'Menu item',
-                                      onTap: () {},
-                                    ),
-                                    const PullDownMenuDivider(),
-                                    PullDownMenuItem(
-                                      title: 'Menu item 2',
-                                      onTap: () {},
-                                    ),
-                                  ],
-                              buttonBuilder: (context, showMenu) => NoteGrid(
-                                    title: note.title,
-                                    content: note.content,
-                                    date: note.modified,
-                                    onTap: () => context.router
-                                        .navigate(NoteRoute(noteId: note.id)),
-                                    onLongPress: showMenu,
-                                  ));
-                        },
-                      );
-                      return null;
+                        );
+                      },
+                      child: _renderNote(note),
+                    ),
+                    cupertino: (context, platform) {
+                      return PullDownButton(
+                          itemBuilder: (context) => [
+                                PullDownMenuItem(
+                                  onTap: () => controller.deleteNote(note),
+                                  title: "Delete",
+                                  isDestructive: true,
+                                ),
+                                PullDownMenuItem(
+                                  onTap: () =>
+                                      controller.addToSelectedNote(note),
+                                  title: "Select",
+                                )
+                              ],
+                          buttonBuilder: (context, showMenu) => Observer(
+                                builder: (context) {
+                                  return _renderNote(note, showMenu);
+                                },
+                              ));
                     },
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             );
           },
         ));
   }
 
-  BottomNavigationBar _renderBottomBar(BuildContext context) {
-    return BottomNavigationBar(
-      onTap: (value) {
-        switch (value) {
-          case 0:
-            break;
-          case 1:
-            context.router.navigate(const NewNoteRoute());
-            break;
-          case 2:
-            break;
-        }
-      },
-      items: [
-        const BottomNavigationBarItem(icon: Icon(EvaIcons.search), label: ""),
-        BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  borderRadius: BorderRadius.circular(10)),
-              child: const Icon(EvaIcons.plus),
-            ),
-            label: ""),
-        const BottomNavigationBarItem(
-          icon: Icon(EvaIcons.moreHorizontalOutline),
-          label: "",
+  Stack _renderNote(Note note, [Function()? showMenu]) {
+    return Stack(
+      children: [
+        NoteGrid(
+          title: note.title,
+          content: note.content,
+          date: note.modified,
+          onTap: () => controller.selectedNotes.isEmpty
+              ? context.router.navigate(NoteRoute(noteId: note.id))
+              : controller.addToSelectedNote(note),
+          onLongPress: showMenu,
         ),
+        if (controller.selectedNotes
+            .where((element) => element.id == note.id)
+            .isNotEmpty)
+          InkWell(
+            onTap: () => controller.addToSelectedNote(note),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.4)),
+              alignment: Alignment.bottomRight,
+              child: const Icon(EvaIcons.checkmarkSquare2),
+            ),
+          ),
       ],
-      showUnselectedLabels: false,
-      showSelectedLabels: false,
-      landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
+    );
+  }
+
+  Widget _renderBottomBar(BuildContext context) {
+    return Observer(
+      builder: (context) {
+        if (controller.selectedNotes.isNotEmpty) {
+          return Container(
+            height: 80,
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).primaryColor,
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Selected (${controller.selectedNotes.length})",
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: controller.bunchDeleteNotes,
+                          icon: const Icon(
+                            EvaIcons.trashOutline,
+                            color: Colors.redAccent,
+                          )),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        }
+        return BottomNavigationBar(
+          onTap: (value) {
+            switch (value) {
+              case 0:
+                break;
+              case 1:
+                context.router.navigate(const NewNoteRoute());
+                break;
+              case 2:
+                break;
+            }
+          },
+          items: [
+            const BottomNavigationBarItem(
+                icon: Icon(EvaIcons.search), label: ""),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Icon(EvaIcons.plus),
+                ),
+                label: ""),
+            const BottomNavigationBarItem(
+              icon: Icon(EvaIcons.moreHorizontalOutline),
+              label: "",
+            ),
+          ],
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+          landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
+        );
+      },
     );
   }
 }
